04208b66a6b18c584a21441c76b9cac2613a1f43
Luke Aguilar <dev@rdxip.com>
2026-02-27 01:04:13 +0000

    Add Yazi file manager & refactor post-create

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "Fixing permissions on mounted volumes ..."
+sudo chown -R vscode:vscode /mnt/mise-data /home/vscode/.local
+
+echo "Configuring zsh ..."
+cat >> ~/.zshrc << 'EOF'
+eval "$(mise activate zsh)" # Enables Mise in shells
+export EDITOR=nano # Preference for Yazi to use nano
+EOF
+
+echo "Enabling mise experimental features ..."
+mise settings experimental=true
+
+echo "Updating apt and installing file utility ..." # Needed for Yazi file manager for file previews
+sudo apt update
+sudo apt install -y file
+
+echo "Post-create setup finished."
